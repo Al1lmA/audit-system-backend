a959b0667c77122d068a1ef82a0eaab0ef97e555
Yana Zhdanova <zhdanova.yana2003@yandex.ru>
2025-05-14 07:59:38 +0300

    Delete audit_api

--- audit_api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Company, Audit, AuditQuestion, AuditHistory, Report
-
-admin.site.register(User)
-admin.site.register(Company)
-admin.site.register(Audit)
-admin.site.register(AuditQuestion)
-admin.site.register(AuditHistory)
-admin.site.register(Report)
